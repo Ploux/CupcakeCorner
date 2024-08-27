1a3214bccafa09ac107504ab012b908466e08b0d
Ploux <peter@humo.golf>
2024-08-27 12:22:19 -0500

    Add address fields and (minimal) address validation.

---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,20 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Peter Loux on 8/27/24.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var order: Order
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    CheckoutView(order: Order())
+}
