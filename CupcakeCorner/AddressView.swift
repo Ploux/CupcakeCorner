f028a28265508837beb03feee9fdf4ff58768615
Ploux <peter@humo.golf>
2024-08-27 12:10:16 -0500

    Add cupcake selection, quantity, toggle switches. Add placeholder second page AddressView

---
+++ CupcakeCorner/AddressView.swift
@@ -0,0 +1,20 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Peter Loux on 8/27/24.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var order: Order
+    
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
--- CupcakeCorner/CupcakeCornerApp.swift
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -15,3 +15,7 @@ struct CupcakeCornerApp: App {
         }
     }
 }
+
+
+
+
